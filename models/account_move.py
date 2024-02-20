69d9da20bf4d4a394a8de8fe1924009463c799ff
micheledic <dicroce.m@stesi.eu>
2024-02-20 14:25:34 +0100

    fix

--- models/account_move.py
+++ models/account_move.py
@@ -7,6 +7,6 @@ import base64
 class AM(models.Model):
     _inherit = 'account.move'
     def _check_balanced(self):
-        if self.env.context.get('import_file',False):
-            self = self.with_context(check_move_validity=False)
-        return super()._check_balanced()
+        if not self.env.context.get('import_file',False):
+           
+            return super()._check_balanced()
